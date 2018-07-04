2dc6b859ec97b0df0896f29d88f93c546c44dc10
Felix Klauke <info@felix-klauke.de>
2018-07-04 08:35:16 +0200

    Return null instead of observable in test code.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- server/src/main/java/de/d3adspace/rebekah/server/provider/RxServerProvider.java
+++ server/src/main/java/de/d3adspace/rebekah/server/provider/RxServerProvider.java
@@ -4,7 +4,6 @@ import de.d3adspace.rebekah.commons.request.Request;
 import de.d3adspace.rebekah.commons.response.Response;
 import io.reactivex.netty.RxNetty;
 import io.reactivex.netty.server.RxServer;
-import rx.Observable;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -30,6 +29,6 @@ public class RxServerProvider implements Provider<RxServer<Request, Response>> {
     @Override
     public RxServer<Request, Response> get() {
         return RxNetty.createTcpServer(serverPort, pipeline -> {
-        }, newConnection -> Observable.just(null));
+        }, newConnection -> null);
     }
 }
